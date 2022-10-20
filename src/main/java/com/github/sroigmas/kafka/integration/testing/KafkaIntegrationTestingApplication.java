cac8b0811c4ed78591eaaca9edba7e060464ef60
Salva Roig <salroima@gmail.com>
2022-10-20 11:47:23 +0200

    Initial commit

---
+++ src/main/java/com/github/sroigmas/kafka/integration/testing/KafkaIntegrationTestingApplication.java
@@ -0,0 +1,13 @@
+package com.github.sroigmas.kafka.integration.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaIntegrationTestingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaIntegrationTestingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/sroigmas/kafka/integration/testing/KafkaIntegrationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sroigmas.kafka.integration.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaIntegrationTestingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
